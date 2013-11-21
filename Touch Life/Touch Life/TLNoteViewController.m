fba7ccf326bb70cab64949f852d8adb87e4d70c5
lc <liuchang129@126.com>
2013-11-21 19:29:29 +0800

    Lc:编辑界面小调整

--- Touch Life/Touch Life/TLNoteViewController.m
+++ Touch Life/Touch Life/TLNoteViewController.m
@@ -248,4 +248,37 @@
     [self.photoVC.view removeFromSuperview];
 }
 
+
+
+
+
+
+-(void)textFieldDidBeginEditing:(UITextView *)textView
+{
+    CGRect frame = textView.frame;
+    int offset = frame.origin.y + 32 - (self.view.frame.size.height - 216.0);//键盘高度216
+    
+    NSTimeInterval animationDuration = 0.30f;
+    [UIView beginAnimations:@"ResizeForKeyboard" context:nil];
+    [UIView setAnimationDuration:animationDuration];
+    
+    //将视图的Y坐标向上移动offset个单位，以使下面腾出地方用于软键盘的显示
+    if(offset > 0)
+        self.view.frame = CGRectMake(0.0f, -offset, self.view.frame.size.width, self.view.frame.size.height);
+    
+    [UIView commitAnimations];
+}
+
+-(BOOL)textFieldShouldReturn:(UITextView *)textView
+{
+    [textView resignFirstResponder];
+    return YES;
+}
+//输入框编辑完成以后，将视图恢复到原始状态
+-(void)textFieldDidEndEditing:(UITextView *)textView
+{
+    self.view.frame =CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+}
+
+
 @end
